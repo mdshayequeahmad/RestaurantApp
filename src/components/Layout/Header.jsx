aee1c0da7405390c4b9b036901b7e82425aa2776
mdshayequeahmad <shayeque01@gmail.com>
2023-06-04 20:29:00 +0530

    Showing and Enabling the delete of Cart Items

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import HeaderCartButton from './HeaderCartButton'
-import mealsImage from "../../assets/image.jpg"
-import classes from './Header.module.css'
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/image.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
-    return (
-        <React.Fragment>
-            <header className={classes.header}>
-                <h1>React Meals</h1>
-                <HeaderCartButton onClick={props.onShowCart} />
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealsImage} alt="A table full of delicious food" />
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import MealItemForm from './MealItemForm'
+import CartContext from '../../../store/cart-context'
 import classes from './MealItem.module.css'
 
 const MealItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`
+    const cartCtx = useContext(CartContext);
+    const price = `$${props.price.toFixed(2)}`;
+
+    const onAddToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    };
     return (
         <li className={classes.meal}>
             <div>
@@ -12,7 +23,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={onAddToCartHandler}/>
             </div>
         </li>
     )
